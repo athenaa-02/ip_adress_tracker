f407830f8cefc2e5dcf0cae553495b78491c8eb2
natia <natia.karamnishvili369@law.tsu.ge>
2025-04-06 22:52:14 +0400

    solved bugs, finished

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ const map_place = document.querySelector('#map')
 const section = document.querySelector('.search_and_heading')
 
 
+const ip_result = document.querySelector('#ip_adress')
+const location_result = document.querySelector('#location')
+const timezone_result = document.querySelector('#timezone')
+const isp_result = document.querySelector('#isp')
+
+
+
 const KEY = 'at_uoj7V9ESPl63DyjAvatjPJmr0yV6Z'
 
 const map = L.map('map').setView([51.505, -0.09], 13);
@@ -14,47 +21,48 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-button.addEventListener('click', () =>{
-    const ip = document.querySelector('#search')
-    console.log(ip)
-})
 
-
+button.addEventListener('click', (event) =>{
+    event.preventDefault()
+    const ip = document.querySelector('#search').value
 
     const getAdress = async() =>{
 
-
         try{
             const response = await axios.get(
-                `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=at_uoj7V9ESPl63DyjAvatjPJmr0yV6Z&ipAddress=8.8.8.8`
+                `https://geo.ipify.org/api/v2/country,city,vpn?apiKey=${KEY}&ipAddress=${ip}`
             )
-            const {location:{lat, lng, country, city, timezone}, isp} = response.data
 
-           console.log(lng, lat, isp, country, city)
-            if(!ip){
-                ip_result.innerContent = ''
-                location_result.innerContent = ''
-                timezone_result.innerContent = ''
-                isp_result.innerContent = ''
-                return;
+        //    console.log(lng, lat, isp, country, city)
+            if(ip){
+
+        const {location:{lat, lng, country, city, timezone}, isp} = response.data
+console.log(country, city, timezone, isp)
+             ip_result.innerText = `${ip}`
+             location_result.innerText = `${country, city}`
+             timezone_result.innerText = `${timezone}`
+             isp_result.innerContent = `${isp}`
+
+             map.setView([lat, lng], 20)
+
+             marker = L.marker([lat, lng])
+
+
         }else{
-                ip_result.innerText = `${ip}`
-                location_result.innerText = `${country, city}`
-                timezone_result.innerText = `${timezone}`
-                isp_result.innerContent = `${isp}`
-
-                map.setView([lng, lat], 20)
+            ip_result.innerContent = ''
+            location_result.innerContent = ''
+            timezone_result.innerContent = ''
+            isp_result.innerContent = ''
+            return;
         }
         }catch{
     
         }
     } 
+    getAdress()
+})
+    
+// 192.212.174.101
 
-
-
-        //     const ip_result = document.querySelectorAll('#ip_adress')
-    //     const location_result = document.querySelectorAll('#location')
-    //     const timezone_result = document.querySelectorAll('#timezone')
-    //     const isp_result = document.querySelectorAll('#isp')
 
     
